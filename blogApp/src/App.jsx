9d80a018f7c1e61512ecbad2bdb2dca8c234d631
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-04-12 04:40:58 +0500

    blog app using react

---
+++ blogApp/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+console.log(import.meta.VITE_APPWRITE_URL);
+  return (
+    <>
+    Blog app using appwrite
+    </>
+  )
+}
+
+export default App
---
+++ blogApp/src/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+appwriteUrl : String(import.meta.VITE_APPWRITE_URL),
+appwriteProjectId : String(import.meta.VITE_APPWRITE_PROJECT_ID ),
+appwriteDatabaseID : String(import.meta.VITE_APPWRITE_DATABASE_ID),
+appwriteCollectionID : String(import.meta.VITE_APPWRITE_COLLECTION_ID ),
+appwriteBucketID : String(import.meta.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default config;
+
+ // import.meta.VITE_APPWRITE_URL
+  // vite kay through jo project banay ga wahan import.meta kar kay env file ko acess
+  //  karain gay otherwise Process.env create rect app kay liay used hota hai 
