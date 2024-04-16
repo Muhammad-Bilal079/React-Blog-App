10e9c0c83ab8d1e6aa7880aac5e49909ea3e15ce
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-04-17 01:51:35 +0500

    bloag app

--- blogApp/src/components/Input.jsx
+++ blogApp/src/components/Input.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { forwardRef, useId } from 'react'
 
-function Input() {
+const Input = forwardRef(function Input(
+  {
+    label,
+    type = "text",
+    className = "",
+    ...props
+  }, ref) {
+
+  const id = useId()
   return (
-    <div>Input</div>
+    <div className='w-full'>
+      {
+        label && <label className='inline-block mb-4 pl-1'
+          htmlFor={id}>
+          {label}
+        </label>
+      }
+
+      <input type={type}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none
+         focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+      ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+      />
+    </div>
   )
-}
+})
+
 
 export default Input
