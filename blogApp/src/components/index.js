75ccdc972040f300cad0963617deebdb046a556d
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-04-19 03:29:53 +0500

    blog app

--- blogApp/src/components/index.js
+++ blogApp/src/components/index.js
@@ -3,11 +3,22 @@ import Footer from "./Footer/Footer";
 import Container from './Container/Container.jsx'
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE  from './RTE.jsx'
+import Signup from './Signup.jsx'
+import Login from './Login.jsx'
+import PostForm from './post-form/PostForm.jsx'
+import AuthLayout from './AuthLayout.jsx'
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    AuthLayout,
+  
 }
---
+++ blogApp/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Signup as SignupComponenet } from '../components'
+
+function SignUp() {
+  return (
+    <div className='py-8'>
+        <SignupComponenet />
+    </div>
+  )
+}
+
+export default SignUp
